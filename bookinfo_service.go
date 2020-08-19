dd373e21e65ac6c2323ad40968c52f3dccb271e0
marcoc22 <mtcanalesmesen@gmail.com>
2020-08-18 18:40:27 -0600

    Add files via upload

---
+++ bookinfo_service.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "context"
+    pb "github.com/arce/gowebservices/booksapp"
+    "github.com/gofrs/uuid"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
+)
+
+type server struct {
+    bookMap map[string]*pb.Book
+}
+
+func (s *server) AddBook(ctx context.Context, in *pb.Book) (*pb.BookID, error) {
+    out, err := uuid.NewV4()
+    if err != nil {
+        return nil, status.Errorf(codes.Internal,
+            "Error while generating Book ID", err)
+    }
+    in.Id = out.String()
+    if s.bookMap == nil {
+        s.bookMap = make(map[string]*pb.Book)
+    }
+    s.bookMap[in.Id] = in
+    return &pb.BookID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+func (s *server) GetBook(ctx context.Context, in *pb.BookID) (*pb.Book, error) {
+    value, exists := s.bookMap[in.Value]
+    if exists {
+        return value, status.New(codes.OK, "").Err()
+    }
+    return nil, status.Errorf(codes.NotFound, "Book does not exist.", in.Value)
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    pb "github.com/arce/gowebservices/booksapp"
+    "google.golang.org/grpc"
+    "log"
+    "net"
+    "os"
+)
+
+func main() {
+    port := os.Getenv("PORT")
+    lis, err := net.Listen("tcp", ":"+port)
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+
+    s := grpc.NewServer()
+    pb.RegisterBookInfoServer(s, &server{})
+
+    log.Printf("Starting gRPC listener on port " + port)
+
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
+}
